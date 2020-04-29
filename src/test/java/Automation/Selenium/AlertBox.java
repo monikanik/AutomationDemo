10d8af0b655656dfbcf5ebc1e604fd7ecc3369a1
monika <dmonika90@gmail.com>
2020-04-29 17:38:42 -0500

    First Commit

---
+++ src/test/java/Automation/Selenium/AlertBox.java
@@ -0,0 +1,42 @@
+package Automation.Selenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertBox {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver", "C:\\softwares\\chromedriver.exe");
+		
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("http://spicejet.com");
+		Thread.sleep(2000);
+		
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		
+		//selecting banglore from dropdown
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+
+		//selecting current date from calender
+				driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight")).click();
+				
+	    //selecting check box
+				driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
+				
+		//clicking on search button
+				
+				driver.findElement(By.id("ctl00_mainContent_btn_FindFlights")).click();
+				
+		//Handling alert box
+				
+			System.out.println(driver.switchTo().alert().getText());
+			
+			driver.switchTo().alert().accept();
+
+	}
+
+}
